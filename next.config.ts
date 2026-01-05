37fd99397a107f0afb79a318358f28ce28e96773
Haymi <hayminib@gmail.com>
2026-01-05 06:25:20 +0000

    No CSP (Content Security Policy) Header Severity: High File: next.config

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,41 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async headers() {
+    const cspHeader = `
+      default-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline';
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' blob: data: https://picsum.photos;
+      font-src 'self';
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+      block-all-mixed-content;
+      upgrade-insecure-requests;
+    `.replace(/\s{2,}/g, ' ').trim();
+
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader,
+          },
+           {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          } 
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
