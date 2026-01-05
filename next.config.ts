4d319b014844afd6fdd50856c545a18836006c13
Haymi <hayminib@gmail.com>
2026-01-05 06:27:48 +0000

    1.1.5.	Missing HTTP Strict Transport Security (HSTS) Header Severity: Me

--- next.config.ts
+++ next.config.ts
@@ -51,6 +51,10 @@ const nextConfig: NextConfig = {
         source: '/:path*',
         headers: [
           {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
             key: 'Content-Security-Policy',
             value: cspHeader,
           },
