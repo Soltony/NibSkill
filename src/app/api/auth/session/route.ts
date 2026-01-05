8069478bf8b2e9b3253bc8557278151ea95b2b29
Haymi <hayminib@gmail.com>
2026-01-05 06:33:12 +0000

    1.1.9.	Sensitive Data Leaked to Client-Side Cookies o	Severity: High o	V

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -1,5 +1,5 @@
 
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 import { getSession } from '@/lib/auth';
 import prisma from '@/lib/db';
 import { cookies } from 'next/headers';
